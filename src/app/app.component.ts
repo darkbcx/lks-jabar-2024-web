d2d16b7368b5a5795c9f921fb8e550b63dc39c6c
darkbcx <darkbcx@yahoo.com>
2024-05-03 01:08:32 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+type ChatMessage = {
+  timestamp: number;
+  message: string;
+  from: 'me' | 'other';
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'lks-jabar-2024-web';
+  myForm!: FormGroup;
+  fb = inject(FormBuilder);
+  wsUrl = '';
+  messages: ChatMessage[] = [
+    {
+      timestamp: 1,
+      message: 'ME',
+      from: 'me',
+    },
+    {
+      timestamp: 2,
+      message: 'OTHER',
+      from: 'other',
+    },
+  ];
+  connected = false;
+  connecting = false;
+
+  constructor() {
+    this.myForm = this.fb.group({
+      message: [null, Validators.required],
+    });
+  }
+
+  formSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    } else {
+      console.log('INVALID');
+    }
+  }
 }
